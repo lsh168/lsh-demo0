fa7b73fff123f90fa159f26d59ea1e9a0930f881
lsh <168500981@qq.com>
2022-07-02 22:41:00 +0800

    Empty project

---
+++ src/main/java/com/lsh/LshApplication.java
@@ -0,0 +1,13 @@
+package com.lsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LshApplication.class, args);
+    }
+
+}
